c649a084ae18b11c01f48efb8a1c8868ae74519d
Ganesh Tiwari <ganesht049@gmail.com>
2025-12-15 14:26:21 +0530

    finalise day 1

---
+++ day-1/hello.py
@@ -0,0 +1,50 @@
+import operator
+import json
+
+# this is the best answer i could come up with
+def read_content(file_name: str ="input.txt"):
+  content = []
+  with open(file=file_name,mode="r", encoding="utf-8") as fp:
+    for line in fp:
+      linecontent = line.strip()
+      content.append(linecontent)
+
+  return content
+
+def turn_dial(
+  starting_point: int,
+  move: str
+) -> [int,int]:
+  operation = operator.sub if move[0] == "L" else operator.add
+  operate_by = int(move[1:], 10)
+  operation_result = operation(starting_point, operate_by)
+  number_complete_turns = 0
+  # breakpoint()
+  if (operation_result > 99 or operation_result < 0):
+    number_complete_turns = abs(operation_result // 100)
+    operation_result = operation_result % 100
+    # breakpoint()
+
+  return [operation_result, number_complete_turns]
+
+
+def main():
+  # content = read_content()
+  content = read_content()
+  # print(content)
+  starting_point = 50
+  nozs = 0
+  for line in content:
+    result, turns_taken = turn_dial(starting_point=starting_point, move=line)
+    if result == 0:
+      nozs = nozs + 1
+    elif turns_taken != 0:
+      nozs = nozs + turns_taken
+      print(json.dumps({"sp": starting_point, "mv": line, "result":result, "tt": turns_taken}))
+    starting_point = result
+
+  print(nozs)
+
+
+if __name__ == "__main__":
+  main()
---
+++ day-1/solution.py
@@ -0,0 +1,24 @@
+# this is the solution i got from github
+with open("input.txt") as f:
+    ls = f.read().strip().split("\n")
+
+# Part 1
+dial = 50
+s = 0
+for l in ls:
+    d, move = l[0], int(l[1:])
+    dial += move if d == "R" else -move
+    dial %= 100
+    s += dial == 0
+print(s)
+
+# Part 2
+dial = 50
+s = 0
+for l in ls:
+    d, move = l[0], int(l[1:])
+    for _ in range(move):
+        dial += 1 if d == "R" else -1
+        dial %= 100
+        s += dial == 0
+print(s)
