ff99bfe3b8b4a767bcb4966726ab8f0799ecba8a
Ganesh Tiwari <ganesht049@gmail.com>
2025-12-22 22:20:03 +0530

    feat: added a github solution and then understood and it and gave my solution

--- day-3/main.py
+++ day-3/main.py
@@ -9,19 +9,24 @@ def read_content(file_name: str = "input"):
 
   return content
 
-def top_two(num_str: str) -> List:
+def all_12_digit_combination(num_str: str) -> List:
+  all_combos = []
+  for num_index, num in enumerate(num_str[:-11]):
+    current_combination = [num]
+    current_charachter_index = num_index
+    while len(current_combination) < 12:
+      leave_remaining = (11 - len(current_combination))
+      remaining_slice = num_str[current_charachter_index+1:(-leave_remaining) if leave_remaining != 0 else None]
+      next_num = max(remaining_slice)
+      current_combination.append(next_num)
+      current_charachter_index = current_charachter_index + (remaining_slice.index(next_num) + 1)
+
+    all_combos.append(int("".join(current_combination)))
+  return all_combos
+
+def all_twos_combinations(num_str: str) -> List:
   previous_items = set()
   all_combinations = set()
-  # top = { "value": -1, "position": -1}
-  # second_top = { "value": -1, "position": -1}
-
-  # for key, num in enumerate(num_str):
-  #   num_value = int(num)
-  #   if num_value > top["value"]:
-  #     second_top = top
-  #     top = { "value": num_value, "position": key }
-  #   elif num_value > second_top["value"]:
-  #     second_top = { "value": num_value, "position": key }
 
   for num in num_str:
     for prev_element in previous_items:
@@ -34,16 +39,23 @@ def top_two(num_str: str) -> List:
 def main():
   content = read_content()
 
+  ################### PART 1 Solution ###########################
+  # maxs = []
+  # for line in content:
+  #   combinations = all_twos_combinations(line)
+  #   maxs.append(max(combinations))
+
+  # print(sum(maxs))
+
+  ################### PART 2 Solution ###########################
   maxs = []
   for line in content:
-    combinations = top_two(line)
-    maxs.append(max(combinations))
+    all_combo = all_12_digit_combination(line)
+    max_value = max(all_combo)
+    maxs.append(max_value)
+  total = sum(maxs)
+  print(total)
 
-  print(sum(maxs))
-    # big_num = int(f"{first}{second}")
-    # total_sum += big_num
-
-  # print(total_sum)
 
 if __name__ == "__main__":
   main()
