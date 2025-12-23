936d5d285a14f17cf4136bf23c9a354cbff1b021
Ganesh Tiwari <ganesht049@gmail.com>
2025-12-23 17:27:25 +0530

    feat: created an output.md with output of matrix

--- day-4/main.py
+++ day-4/main.py
@@ -1,3 +1,4 @@
+from functools import total_ordering
 from dataclasses import dataclass
 from typing import List, Tuple
 
@@ -20,18 +21,10 @@ def get_adjacent_points(coordinate: Coordinate, matrix: List[List[Coordinate]]) 
   adjacent_points = [(coordinate.x + dx, coordinate.y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1),(-1,-1),(-1,1),(1,-1),(1,1)]]
   return [Coordinate(x=point[0], y=point[1], value=matrix[point[0]][point[1]].value) for point in adjacent_points if point[0] >= 0 and point[0] < len(matrix) and point[1] >= 0 and point[1] < len(matrix[0])]
 
-if __name__ == "__main__":
-  content = read_content()
-  matrix: List[List[Coordinate]] = []
-  for line_index, line in enumerate(content):
-    line_list: List[Coordinate] = []
-    for char_index, char in enumerate(line):
-      coordinate = Coordinate(x=line_index, y=char_index, value=char)
-      line_list.append(coordinate)
-    matrix.append(line_list)
-
-  can_be_fklfted_cnt = 0
-  for line in matrix:
+def get_can_be_fklfted_matrix(matrix: List[List[Coordinate]]) -> List[List[Coordinate]]:
+  can_be_fklfted_matrix: List[List[Coordinate]] = []
+  for line_index, line in enumerate(matrix):
+    can_be_fklfted_line: List[Coordinate] = []
     for coordinate in line:
       if coordinate.value != "@":
         # if it is not a paper roll, skip it
@@ -50,6 +43,56 @@ if __name__ == "__main__":
       # if the number of adjacent paper rolls is less than 4, then
       # the paper roll can be forklifted
       if no_of_adj_rolls < 4:
-        can_be_fklfted_cnt += 1
+        can_be_fklfted_line.append(coordinate)
+    can_be_fklfted_matrix.append(can_be_fklfted_line)
 
-  print(can_be_fklfted_cnt)
+  return can_be_fklfted_matrix
+
+def part1_solution(matrix: List[List[Coordinate]]):
+  can_be_fklfted_matrix = get_can_be_fklfted_matrix(matrix)
+  count = 0
+  for line in can_be_fklfted_matrix:
+    for coordinate in line:
+      count += 1
+  print("Count of forklifted paper rolls:", count)
+
+def part2_solution(matrix: List[List[Coordinate]]):
+  forklifting_done = False
+  iteration = 0
+  total_forklifted_count = 0
+  while not forklifting_done:
+    can_be_fklfted_matrix = get_can_be_fklfted_matrix(matrix)
+    forklifted_count = 0
+    for line in can_be_fklfted_matrix:
+      for coordinate in line:
+        matrix[coordinate.x][coordinate.y].value = "x"
+        forklifted_count += 1
+
+    print(f"Iteration: {iteration}, Forklifted count: {forklifted_count}")
+    iteration += 1
+    total_forklifted_count += forklifted_count
+    forklifting_done = forklifted_count == 0
+
+  for line in matrix:
+    for coordinate in line:
+      if coordinate.value == "x":
+        coordinate.value = "."
+
+  print("Final matrix:")
+  for line in matrix:
+    print("".join([coordinate.value for coordinate in line]))
+
+  print("Total forklifted count:", total_forklifted_count)
+
+if __name__ == "__main__":
+  content = read_content()
+  matrix: List[List[Coordinate]] = []
+  for line_index, line in enumerate(content):
+    line_list: List[Coordinate] = []
+    for char_index, char in enumerate(line):
+      coordinate = Coordinate(x=line_index, y=char_index, value=char)
+      line_list.append(coordinate)
+    matrix.append(line_list)
+
+  # part1_solution(matrix)
+  part2_solution(matrix)
