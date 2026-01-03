362b4b01353ff3d1b81588a714cc8ff4bd2691c5
Ganesh Tiwari <ganesht049@gmail.com>
2026-01-03 22:12:42 +0530

    feat: wrote a simple algo

---
+++ day-7/algo.md
@@ -0,0 +1,7 @@
+1. In the first row check for `S` and immediately in the second row put a
+`|` below it.
+2. Do not do anything in the second row, Either do not do anything in the rows whose index are odd numbers because the action on them is taken from the rows which have even indexes.
+3. In the even indexed rows from below that 
+    i. For each element in row, if element right above it is `|` 
+        a. If current element is a `^` then mark adjacent elements in its row as well as in the row below it as `|`, and increase the ray split count.
+        b. If the current element is `.` then mark the current element as well as the element right below it as `|`.
--- day-7/main.py
+++ day-7/main.py
@@ -13,31 +13,7 @@ class Coordinate:
   y: int
 
 def solution_1(content: List[str]):
-  ray_uninitialised = True
-  ray_map = {}
-  for line_index, line in enumerate(content):
-    for char_index, char in enumerate(line):
-      if ray_uninitialised and char == start:
-        print(f"Starting found at {line_index}:{char_index}")
-        ray_uninitialised = False
-        ray_map[line_index] = [char_index]
-
-      if not ray_uninitialised:
-        pre_line_ray_points = ray_map[line_index - 1]
-        new_line = line
-        if char_index in pre_line_ray_points:
-          match(char):
-            case ".":
-              # change this point to ray
-              new_line = line[:char_index] + "|" + line[char_index + 1:]
-              ray_map[line_index].append(char_index)
-            case "^":
-              # change below two dots to left and right of this to rays
-              pass
-
-
-
-
+  solved_matrix = current_matrix = [list(x) for x in content]
 
   return 0
 
